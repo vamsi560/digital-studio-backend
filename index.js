2a9978abe4c949ccd21c80d404c28552f85fad97
vamsi560 <145781997+vamsi560@users.noreply.github.com>
2025-07-16 10:37:39 +0000

    Update index.js

--- index.js
+++ index.js
@@ -335,6 +335,9 @@ app.post('/api/generate-code', upload.array('screens'), async (req, res) => {
         res.status(500).json({ error: 'An error occurred on the server during code generation.' });
     }
 });
+app.get('/', (req, res) => {
+  res.send('Backend server is running!');
+});
 
 // Export the app for Vercel
 module.exports = app;
